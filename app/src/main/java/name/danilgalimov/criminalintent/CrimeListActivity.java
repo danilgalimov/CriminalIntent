67c3c5ef78eb3f510dddbac06c401f46d6670188
Danil Galimov <danilgalimov26@gmail.com>
2022-02-27 22:00:55 +0500

    Обработка касаний на элементы RecyclerView.

---
+++ app/src/main/java/name/danilgalimov/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package name.danilgalimov.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
